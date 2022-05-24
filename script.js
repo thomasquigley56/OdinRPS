26c63507402669065a7262dc991eac1ccfd3f462
thomasquigley56 <thomasquigley@my.unt.edu>
2022-05-24 09:33:01 -0500

    Add game function

--- script.js
+++ script.js
@@ -9,8 +9,8 @@ function playRound(playerSelection, computerSelection){
     const pChoice = playerSelection.charAt(0).toUpperCase() + playerSelection.substr(1).toLowerCase();
     const cChoice = computerSelection.charAt(0).toUpperCase() + computerSelection.substr(1).toLowerCase();
 
-    const win = `${pChoice} beats ${cChoice}. You win!`;
-    const lose = `${cChoice} beats ${pChoice}. You lose!`;
+    const win = `You win! ${pChoice} beats ${cChoice}.`;
+    const lose = `You lose! ${cChoice} beats ${pChoice}.`;
 
     if(pChoice === cChoice) return "It's a tie!";
 
@@ -32,3 +32,21 @@ function playRound(playerSelection, computerSelection){
     }
 }
 
+function game(){
+    let pWins = 0; //Player wins
+    let cWins = 0; //Computer wins
+    let pChoice;
+    let result;
+    for(let i = 0; i< 5; ++i){
+        pChoice = prompt("Choose Rock, Paper, or Scissors: ");
+        result = playRound(pChoice, computerPlay());
+        if(result.charAt(4) === 'w') pWins++;
+        else if(result.charAt(4) === 'l') cWins++;
+        console.log(result);
+    }
+    if(pWins>cWins) console.log("Player Wins!");
+    else if(cWins>pWins) console.log("Computer Wins!");
+    else console.log("It's a tie!");
+}
+
+game();
